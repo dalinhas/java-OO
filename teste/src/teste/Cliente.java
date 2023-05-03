84d5d6d96d65d975b5c85f716a58dab373416ece
Rosalia <104944125+dalinhas@users.noreply.github.com>
2023-05-02 21:08:12 -0300

    alteração mais um cliente

--- teste/src/teste/Cliente.java
+++ teste/src/teste/Cliente.java
@@ -7,6 +7,7 @@ public class Cliente {
 	String cpf;
 	int senha;
 	String profissão;
+	
 
 
 
--- teste/src/teste/Conta1.java
+++ teste/src/teste/Conta1.java
@@ -7,7 +7,7 @@ public class Conta1 {
 		Cliente contaDoMarcos = new Cliente();
 		contaDoMarcos.nome = "Marcos Augusto";
 		contaDoMarcos.idade = 40;
-		contaDoMarcos.cpf = "4567";
+		contaDoMarcos.cpf = "313";
 		contaDoMarcos.profissão = "Programador";
 		
 		// Aqui temos o uso da concatenação e os resultados retornados.
@@ -15,5 +15,24 @@ public class Conta1 {
 		System.out.println("Aqui é a idade: " + contaDoMarcos.idade);
 		System.out.println("Aqui é o CPF: " + contaDoMarcos.cpf);
 		System.out.println("Aqui é sua profissão: " + contaDoMarcos.profissão);
+		
+		Cliente contaDoJosue = new Cliente();
+		contaDoJosue.nome = "Josue Silva";
+		contaDoJosue.idade = 35;
+		contaDoJosue.cpf = "55555555555";
+		contaDoJosue.profissão = "Pipoqueiro";
+		
+		
+		// Aqui temos o uso da concatenação e os resultados retornados.
+		System.out.println("Aqui é o nome: " + contaDoJosue.nome);
+		System.out.println("Aqui é a idade: " + contaDoJosue.idade);
+		System.out.println("Aqui é o CPF: " + contaDoJosue.cpf);
+		System.out.println("Aqui é sua profissão: " + contaDoJosue.profissão);
 	}
+	
+	
+	
+	
+	
+	
 }
